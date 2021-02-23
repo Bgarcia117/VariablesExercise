20c5f326ff60e6f46536848bae97ac2373cd055a
Bryant Garcia <bryantgarcia@BCs-MacBook-Pro-2.local>
2021-02-23 12:46:38 -0600

    Complete Variables Exercise

---
+++ variablesExercise.js
@@ -0,0 +1,41 @@
+// #1
+// A variables that does not change is the: const variable
+
+// #2
+let num = 20;
+
+// #3
+num = 15;
+
+// #4 Both Ways
+num = num + 7;
+num += 7;
+
+// #5
+num--;
+
+// #6
+num = "This is not a number";
+
+// #7
+let blank;
+
+// #8
+console.log(blank);
+
+// #9
+blank = null
+
+// BONUS
+
+// #1
+const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+
+// #2
+console.log(quarter, dime, nickel, penny);
+
+// #3
+let letters = "qwertyuiop";
+
+// #4
+console.log(letter(letters.length - 1))
